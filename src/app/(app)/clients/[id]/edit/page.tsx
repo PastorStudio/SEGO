1bcb0a514f423ea56e240a46601f60e74f184de0
PastorStudio <mmorenofrias06@gmail.com>
2025-09-03 02:16:01 -0400

    Update page.tsx
    update

--- src/app/(app)/clients/[id]/edit/page.tsx
+++ src/app/(app)/clients/[id]/edit/page.tsx
@@ -1,20 +1,25 @@
-
-import { getClient } from "@/lib/data";
-import { notFound } from "next/navigation";
-import { PageHeader } from "@/components/page-header";
-import { ClientForm } from "../../_components/client-form";
-
-export default async function EditClientPage({ params }: { params: { id: string } }) {
-  const client = await getClient(params.id);
-
-  if (!client) {
-    notFound();
-  }
-
-  return (
-    <>
-      <PageHeader title="Editar Cliente" />
-      <ClientForm client={client} />
-    </>
-  );
-}
+1 import { getClient } from "@/lib/data";
+    2 import { notFound } from "next/navigation";
+    3 import { PageHeader } from "@/components/page-header";
+    4 import { ClientForm } from "../../_components/client-form";
+    5 
+    6 interface EditClientPageProps {
+    7   params: {
+    8     id: string;
+    9   };
+   10 }
+   11 
+   12 export default async function EditClientPage({ params }: EditClientPageProps) {
+   13   const client = await getClient(params.id);
+   14 
+   15   if (!client) {
+   16     notFound();
+   17   }
+   18 
+   19   return (
+   20     <>
+   21       <PageHeader title="Editar Cliente" />
+   22       <ClientForm client={client} />
+   23     </>
+   24   );
+   25 }
