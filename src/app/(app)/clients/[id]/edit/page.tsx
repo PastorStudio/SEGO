8b7cd7eded6f0d780a2cbb4d6e536f410def819d
PastorStudio <mmorenofrias06@gmail.com>
2025-09-03 12:03:28 -0400

    Update page.tsx
    update

--- src/app/(app)/clients/[id]/edit/page.tsx
+++ src/app/(app)/clients/[id]/edit/page.tsx
@@ -4,7 +4,11 @@ import { notFound } from "next/navigation";
 import { PageHeader } from "@/components/page-header";
 import { ClientForm } from "../../_components/client-form";
 
-export default async function EditClientPage({ params }: { params: { id: string } }) {
+type EditClientPageProps = {
+    params: { id: string };
+};
+
+export default async function EditClientPage({ params }: EditClientPageProps) {
   const client = await getClient(params.id);
 
   if (!client) {
