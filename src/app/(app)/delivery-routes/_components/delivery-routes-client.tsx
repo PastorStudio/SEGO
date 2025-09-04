9d97a87ccfd292e55d95fa5e0c5d23fc9c37914b
PastorStudio <mmorenofrias06@gmail.com>
2025-09-03 21:59:23 -0400

    Update delivery-routes-client.tsx
    update

--- src/app/(app)/delivery-routes/_components/delivery-routes-client.tsx
+++ src/app/(app)/delivery-routes/_components/delivery-routes-client.tsx
@@ -2,7 +2,8 @@
 
 'use client'
 
-import { type WarehouseRequest, type Project, type User, addCommentToWarehouseRequest } from "@/lib/data";
+import type { WarehouseRequest, Project, User } from "@/lib/definitions";
+import { addCommentToWarehouseRequest } from "@/lib/data";
 import { Card, CardContent } from "@/components/ui/card";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 import { Package, NotebookText, Truck } from "lucide-react";
